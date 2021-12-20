aca961aa87d8101fc7f320df8a3cf088788546fc
Paloma Lozano <palomalogu9@gmail.com>
2021-12-20 14:19:22 +0100

    created the gallery with javascript and the overlay

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,32 @@ function crearGaleria() {
     const imagen = document.createElement('picture');
     imagen.innerHTML = `<img src="assets/images/img/thumb/${i}.jpg" type="image/jpg">
     <img loading="lazy" width="200" height="300" src="assets/images/img/thumb/${i}.jpg" alt="galería de imágenes"`;
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
     galeria.appendChild(imagen);
   }
+}
+
+function mostrarImagen(id) {
+  const imagen = document.createElement('picture');
+  imagen.innerHTML = `<img src="assets/images/img/grande/${id}.jpg" type="image/jpg">
+    <img loading="lazy" width="200" height="300" src="assets/images/img/grande/${id}.jpg" alt="galería de imágenes"`;
+  const overlay = document.createElement('DIV');
+  overlay.appendChild(imagen);
+  overlay.classList.add('overlay');
+
+  const cerrarModal = document.createElement('P');
+  cerrarModal.textContent = 'X';
+  cerrarModal.classList.add('btn-cerrar');
+  cerrarModal.onclick = function () {
+    const body = document.querySelector('body');
+    body.classList.remove('fijar-body');
+    overlay.remove();
+  };
+  overlay.appendChild(cerrarModal);
+
+  const body = document.querySelector('body');
+  body.appendChild(overlay);
+  body.classList.add('fijar-body');
 }
