1f102ae2d8be3b5d22508445e0c507fefad9f9d0
Paloma Lozano <palomalogu9@gmail.com>
2021-12-20 13:47:28 +0100

    added images to gallery

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello');
+document.addEventListener('DOMContentLoaded', function () {
+  iniciarApp();
+});
+
+function iniciarApp() {
+  crearGaleria();
+}
+
+function crearGaleria() {
+  const galeria = document.querySelector('.galeria-imagenes');
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `<img src="assets/images/img/thumb/${i}.jpg" type="image/jpg">
+    <img loading="lazy" width="200" height="300" src="assets/images/img/thumb/${i}.jpg" alt="galería de imágenes"`;
+    galeria.appendChild(imagen);
+  }
+}
